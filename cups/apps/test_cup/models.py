779fe73b59082ecd56f9ffa0f92138ff49384f0f
Sergey Zlobov <sergey.zlobov@mediasapiens.co>
2013-11-23 18:14:52 +0200

    q

--- cups/apps/test_cup/models.py
+++ cups/apps/test_cup/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-# from django.db.models.signals import post_delete, post_save, post_init
-# from django.dispatch import receiver
+from django.db.models.signals import post_delete, post_save, post_init
+from django.dispatch import receiver
 
 # Create your models here.
 class Person(models.Model):
@@ -27,15 +27,15 @@ class RequestDB(models.Model):
     def __unicode__(self):
         return self.date.strftime("%a, %d %b %Y %H:%M:%S")
 
-#
-# @receiver(post_init,sender = Person)
-# def callback_init(sender, **kwargs):
-#     print ('>>>>>>>>>>>>>>> Created! <<<<<<<<<<<<<<<<')
-#
-# @receiver(post_delete,sender = Person)
-# def callback_delete(sender, **kwargs):
-#     print ('>>>>>>>>>>>>>>> Deleted! <<<<<<<<<<<<<<<<')
-#
-# @receiver(post_save,sender = Person)
-# def callback_change(sender, **kwargs):
-#     print ('>>>>>>>>>>>>>>> Changed! <<<<<<<<<<<<<<<<')
+
+@receiver(post_init,sender = Person)
+def callback_init(sender, **kwargs):
+    print ('>>>>>>>>>>>>>>> Created! <<<<<<<<<<<<<<<<')
+
+@receiver(post_delete,sender = Person)
+def callback_delete(sender, **kwargs):
+    print ('>>>>>>>>>>>>>>> Deleted! <<<<<<<<<<<<<<<<')
+
+@receiver(post_save,sender = Person)
+def callback_change(sender, **kwargs):
+    print ('>>>>>>>>>>>>>>> Changed! <<<<<<<<<<<<<<<<')
