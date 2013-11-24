92f5ec2cb10643c341624346f5af75a276b1d538
Sergey Zlobov <sergey.zlobov@mediasapiens.co>
2013-11-24 22:13:42 +0200

    ticket 3: add test. Test dont pass.

--- cups/apps/test_cup/tests.py
+++ cups/apps/test_cup/tests.py
@@ -1,4 +1,5 @@
 from django_webtest import WebTest
+from django.core.urlresolvers import reverse
 from models import *
 
 class TestCase(WebTest):
@@ -9,5 +10,21 @@ class TestCase(WebTest):
         """
         tests, that 1 ticket page exists and show correct data
         """
-        resp = self.app.get('/')
+        resp = self.app.get(reverse('main'))
         assert resp.status == '200 OK'
+
+    def test_t3_middleware_m_page(self):
+        """
+        test that main page has a link to pages with requests
+        """
+        resp = self.app.get(reverse('main'))
+        go_to_requests = resp.click('requests')
+        assert go_to_requests.status == '200 OK'
+
+    def test_t3_middleware_req_page_exists(self):
+        resp = self.app.get(reverse('requests'))
+        assert resp.status == '200 OK'
+
+    def test_t3_middleware_req_exists_on_its_page(self):
+        resp = self.app.get(reverse('requests'))
+        assert resp.mustcontain('<li>')
