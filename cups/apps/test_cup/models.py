3adc3c024cbcca4566d1bed7cd2d94eac034c16b
Sergey Zlobov <sergey.zlobov@mediasapiens.co>
2013-11-24 13:09:33 +0200

    ticket 1:
    add extra fields to model
    add bootstrap

--- cups/apps/test_cup/models.py
+++ cups/apps/test_cup/models.py
@@ -4,8 +4,14 @@ class Person(models.Model):
 
         name = models.CharField(max_length= 30)
         last_name = models.CharField(max_length= 30)
+        date_of_birth = models.DateField()
+        email = models.EmailField()
+        jabber = models.EmailField(blank=True)
+        skype = models.CharField(max_length=20,blank=True)
         bio = models.TextField()
         contacts = models.TextField()
+        other_contacts = models.TextField(blank=True)
+        photo = models.ImageField(blank=True,upload_to='.',null=True,max_length=100, )
 
         def __unicode__(self):
-                return self.name
+            return '{} {}'.format(self.name,self.last_name)
